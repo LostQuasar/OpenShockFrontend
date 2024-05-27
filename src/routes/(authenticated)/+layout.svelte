cb02fc5585c7c16b5e481b5e229e84e2bcf0489a
LostQuasar <rionlion100@gmail.com>
2024-05-26 22:06:30 -0400

    Fix button spacing

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -3,8 +3,8 @@
 </script>
 
 {#if !$UserSelfStore}
-  <div class="container h-full mx-auto flex justify-center items-center">
-    <h1 class="text-4xl">You need to be logged in to access this page</h1>
+  <div class="container h-full mx-auto flex flex-col justify-center items-center">
+    <h1 class="text-4xl my-4">You need to be logged in to access this page</h1>
     <a href="/login" class="btn variant-filled-primary">Login</a>
   </div>
 {:else}
